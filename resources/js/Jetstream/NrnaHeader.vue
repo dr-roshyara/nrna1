f2d8dbba367e2fa5011d71072da376f6f4ba8aa9
= <mathematikboy@yahoo.com>
2021-07-17 21:04:28 +0200

    Notice Controller

--- resources/js/Jetstream/NrnaHeader.vue
+++ resources/js/Jetstream/NrnaHeader.vue
@@ -4,15 +4,16 @@
             <div class="flex flex-start spac-x-4 items-center ">
                 <!-- <div id="logo" style="background-image: url('/logo-2.png')"> </div> -->
                 <div id="logo" class="w-24 h-20"> 
-                    <img class="object-cover" src="/images/logo-2.png" width="200" height="200" />
+                  <a href="/">  <img class="object-cover" src="/images/logo-2.png" width="200" height="200" />
+                  </a>
                 </div> 
                 <!-- <div> <link href="#"> <img  :src ="image1"  style="width:100%;" alt="slogan" /> </link> </div>   -->
                 <div id="slogan" class="ml-4 px-2 w-96">  
-                    <img class="object-cover" src="/images/slogan.png" />
+                  <a href="/">  <img class="object-cover" src="/images/slogan.png" /> </a>
                 </div>  
             </div> 
              <div class="hidden sm:block m-auto text-white font-bold text-xl"> 
-                 NRNA GERMANY 
+                <a href="/"> NRNA GERMANY </a>
              </div>
              <div class="w-44 m-auto h-32">
                <img class="object-cover" src="/images/germanynepal.png" width="200" height="200"/>
