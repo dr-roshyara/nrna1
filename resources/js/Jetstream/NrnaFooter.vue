392833e8a191c5cae7a630886803a75f73141505
= <mathematikboy@yahoo.com>
2021-07-19 01:42:01 +0200

    links created

--- resources/js/Jetstream/NrnaFooter.vue
+++ resources/js/Jetstream/NrnaFooter.vue
@@ -1,12 +1,20 @@
 <template>
        <!-- <div class=" flex flex-left  p-4  bg-blue-600 md: w-full rounded border border-blue-100"> -->
         <div class="flex flex-col w-full bg-blue-600 min-h-10 mt-200 rouned border border-blue-100 p-6 mb-2">
+          <div class="flex flex-rwow justify-center m-auto space-x-4 py-2 mb-2">
+                    <inertia-link href="/candidacies/index" class=" p-2 mx-2 bg-red-50 font-bold text-gray-900 border-right rounded-sm"> List of Candidates </inertia-link>
+                    <inertia-link href="/candidacy/create" class=" p-2 bg-red-50 font-bold text-gray-900 rounded border border-right "> Candidacy Form </inertia-link>
+                    <inertia-link href="/posts/index" class=" p-2  mx-2 bg-red-50 font-bold text-gray-900 border-right rounded-sm"> Name of Posts</inertia-link>
+                     <inertia-link href="/notices/index" class=" p-2   mx-2 bg-red-50 font-bold text-gray-900 rounded-sm"> List of Notices</inertia-link>
+
+        </div> 
         <div class="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8 text-center">
             <div class="text-white text-sm font-bold">
                 Copyright &copy;2021 NRNA Germany. All Rights Reserved by NRNA
                 Germany
-            </div>
+            </div> 
         </div>
+        
     </div>  
 </template>
 <script>
