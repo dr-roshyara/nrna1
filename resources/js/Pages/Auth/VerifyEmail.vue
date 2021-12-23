62c4b3c7ae4ade4675f843c33b2650280d0d20fb
Dr. Nab Roshyara <mathematikboy@yahoo.com>
2021-12-23 18:13:49 +0100

    email verification page

--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -5,12 +5,15 @@
         </template>
 
         <div class="mb-4 text-sm text-gray-600">
-            Thanks for signing up! You have been registerd now. We have just sent you an email. 
+            <p > 
+                <span class="text-gray-900 font-semibold"> 
+                Thanks for signing up!</span> <br> 
+             You have been registerd now. We have just sent you an email. 
             Before getting started, could you please verify your email address?. For this you need to check 
             your mailbox and click  on the link we just emailed to you. 
-            If you didn't receive the email, we will gladly send you another.
-            <p class="mt-2 mb-2 text-gray-800"> 
-                यहाँले अफुलाई रजिस्टर गर्नु भएको बिशेष धन्यवाद छ। 
+            If you didn't receive the email, we will gladly send you another.</p> 
+            <p class="mt-2 mb-2 text-gray-800 text-"> 
+                यहाँले आफुलाईलाई रजिस्टर गर्नु भएकोमा  बिशेष धन्यवाद छ। 
                 भर्खरै हामीले तपाईं लाई एउटा ईमेल पठाएका छौ । 
                 त्यो ईमेल मा यहाँको ईमेल ठेगाना ठीक छ कि छैन भनेर जाँच गर्न लाई एउटा लिन्क पनि पठाइएको छ। 
                 आफ्नो ईमेल खोलेर यहाँले  त्यो लिन्कमा क्लिक्क गर्नु हुने छ । 
