76a3b853933a262fd609eec50e592aafef553992
Dr. Nab Roshyara <mathematikboy@yahoo.com>
2021-12-23 17:53:30 +0100

    email conformation edited

--- database/migrations/2020_05_21_100000_create_teams_table.php
+++ database/migrations/2020_05_21_100000_create_teams_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddMoreTablesToCandidaciesTable extends Migration
+class CreateTeamsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,12 @@ class AddMoreTablesToCandidaciesTable extends Migration
      */
     public function up()
     {
-        Schema::table('candidacies', function (Blueprint $table) {
-            //
-              //$table->timestamps(); 
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->index();
+            $table->string('name');
+            $table->boolean('personal_team');
+            $table->timestamps();
         });
     }
 
@@ -26,8 +29,6 @@ class AddMoreTablesToCandidaciesTable extends Migration
      */
     public function down()
     {
-        Schema::table('candidacies', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('teams');
     }
 }
--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -5,7 +5,18 @@
         </template>
 
         <div class="mb-4 text-sm text-gray-600">
-            Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another.
+            Thanks for signing up! You have been registerd now. We have just sent you an email. 
+            Before getting started, could you please verify your email address?. For this you need to check 
+            your mailbox and click  on the link we just emailed to you. 
+            If you didn't receive the email, we will gladly send you another.
+            <p class="mt-2 mb-2 text-gray-800"> 
+                यहाँले अफुलाई रजिस्टर गर्नु भएको बिशेष धन्यवाद छ। 
+                भर्खरै हामीले तपाईं लाई एउटा ईमेल पठाएका छौ । 
+                त्यो ईमेल मा यहाँको ईमेल ठेगाना ठीक छ कि छैन भनेर जाँच गर्न लाई एउटा लिन्क पनि पठाइएको छ। 
+                आफ्नो ईमेल खोलेर यहाँले  त्यो लिन्कमा क्लिक्क गर्नु हुने छ । 
+                 अनी यहाँको ईमेल सही भएको प्रमाणित हुने छ। 
+                 त्यस्पछी आफ्नो ईमेल र पासवर्ड प्रयोग गरेर लग इन गर्न सक्नु हुनेछ। 
+            </p>
         </div>
 
         <div class="mb-4 font-medium text-sm text-green-600" v-if="verificationLinkSent" >
