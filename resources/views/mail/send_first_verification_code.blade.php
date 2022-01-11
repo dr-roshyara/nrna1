3eab999d43778d1e4abf472ee93ad599d716a258
Dr. Nab Roshyara <mathematikboy@yahoo.com>
2022-01-11 23:11:25 +0100

    email changed

--- resources/views/mail/send_first_verification_code.blade.php
+++ resources/views/mail/send_first_verification_code.blade.php
@@ -4,14 +4,33 @@
 # First Voting Code 
 <p> 
 Namaskar  {{$user->name}}! <br> 
-Your voting code is given below. 
+Your first voting code is given below. 
 With this code you can open the voting form. 
-Please go to the url where you are supposed to give the code.</p> 
+Please go to the previous form  to submit the code.<br> 
+ <span style="font-weight:bold;"> Note:</span>  You can also click the following link
+ and give the voting code</p> 
+<p style="margin-top: 2px; margin-bottom:2px; text-align:center;"> 
+ <a href="{{ route('code.create') }}" 
+ style="color:#1E90FF; font-weight:bold; "> 
+  Click here to verify your code </a> 
+</p> 
+
+
 <p> नमस्कार! <br> 
 तपाइकाे भोटिङ कोड तल लेखिएको छ। <br>
-कृपया यो कोड लाइ   अघि देखाएको फर्ममा लेख्नुहोस र लेखि सकेपछि त्यसै 
-ठाउमा वनाएकाे वटन थिच्नुहाेस ।  
+कृपया त्यो कोड लाइ   अघि देखाएको फर्ममा लेख्नुहोस र लेखि सकेपछि त्यसै 
+ठाउमा वनाएकाे वटन थिच्नुहाेस ।  <br>
+<span style="font-weight:bold;"> पुनश्चः </span> तल उल्लेखित  लिन्कमा क्लीक गरेर पनि 
+अघि देखाएको पर्ममा पुग्न सकिन्छ। 
+
 </p> 
+
+<p style="margin-top: 2px; margin-bottom:2px; text-align:center;"> 
+ <a href="{{ route('code.create') }}" 
+ style="color:#1E90FF; font-weight:bold; "> 
+  मतदान गर्नको लागि  यहाँ क्लिक गर्नुहोस। </a> 
+</p> 
+
 @component('mail::button', ['url' => ''])
  {{ $code }} 
 @endcomponent
--- resources/views/mail/send_second_verification_code.blade.php
+++ resources/views/mail/send_second_verification_code.blade.php
@@ -3,11 +3,29 @@
 <p> 
 Namaskar  {{$user->name}}! <br> 
 Thank you for your vote. Your code to conform your vote is given below. 
-Please go to the url where you are supposed to give the code.</p> 
+Please go to the url where you are supposed to give the code. 
+<br> 
+<span style="font-weight:bold;"> Note:</span>  
+ You can also click the following link
+ and give the voting code
+ </p> 
+<p style="margin-top: 2px; margin-bottom:2px; text-align:center;"> 
+ <a href="{{ route('vote.verfiy') }}" 
+ style="color:#1E90FF; font-weight:bold; "> 
+  मतदान पुर्ण रूपमा सेभ गर्नलाइ  यहाँ क्लिक गर्नुहोस। </a> 
+</p> 
 <p> नमस्कार! <br> 
 यहाँले भोट गर्ने काम लगभग सकाइसक्नु भएको छ।  <br>
 कृपया अव तल लेखिएको कोड लाइ  अघि  भर्खरै देखाएको फर्ममा लेख्नुहोस र लेखि सकेपछि आफ्नो भोट सेभ गर्नलाइ त्यसै 
 ठाउमा वनाएकाे वटन थिच्नुहाेस ।  
+<br>
+<span style="font-weight:bold;"> पुनश्चः </span> तल उल्लेखित  लिन्कमा क्लीक गरेर पनि 
+अघि देखाएको पर्ममा पुग्न सकिन्छ। 
+</p> 
+<p style="margin-top: 2px; margin-bottom:2px; text-align:center;"> 
+ <a href="{{ route('vote.verfiy') }}" 
+ style="color:#1E90FF; font-weight:bold; "> 
+  मतदान पुर्ण रूपमा सेभ गर्नलाइ  यहाँ क्लिक गर्नुहोस। </a> 
 </p> 
 
 @component('mail::button', ['url' => ''])
