147f6b18534755768d028784ac9b70b490cba325
dr-roshyara <mathematikboy@yahoo.com>
2022-06-07 13:08:53 +0200

    google analytics

--- app/Services/Google.php
+++ app/Services/Google.php
@@ -9,7 +9,16 @@ class Google
 
     function __construct()
     {
-        $client = new \Google_Client();
+        $_wd_credential    = storage_path('credentials/');
+        $tokenPath         = $_wd_credential."google_token.json";
+        $json_file_name     =$_wd_credential.'client_secret_roshyara_local.json';
+        //start here
+         $this->google_login();
+        // $this->client =$this->getClient($json_file_name,$tokenPath);
+    }
+    public function google_login(){
+
+         $client = new \Google_Client();
         $client->setClientId(config('services.google.client_id'));
         $client->setClientSecret(config('services.google.client_secret'));
         $client->setRedirectUri(config('services.google.redirect_uri'));
@@ -19,6 +28,55 @@ class Google
         $client->setIncludeGrantedScopes(config('services.google.include_granted_scopes'));
         $this->client = $client;
     }
+    public function getClient($credential_file, $tokenPath)
+    {
+        $client = new \Google_Client();
+        $client->setApplicationName('Google Calendar API PHP Quickstart');
+        $client->setScopes(config('services.google.scopes'));
+        $client->setAuthConfig($credential_file);
+        $client->setAccessType('offline');
+        $client->setPrompt('select_account consent');
+        $client->setRedirectUri(config('services.google.redirect_uri'));
+        // Load previously authorized token from a file, if it exists.
+        // The file token.json stores the user's access and refresh tokens, and is
+        // created automatically when the authorization flow completes for the first
+        // time.
+        // $tokenPath = 'token.json';
+        if (file_exists($tokenPath)) {
+            $accessToken = json_decode(file_get_contents($tokenPath), true);
+            $client->setAccessToken($accessToken);
+        }
+
+        // If there is no previous token or it's expired.
+        if ($client->isAccessTokenExpired()) {
+            // Refresh the token if possible, else fetch a new one.
+            if ($client->getRefreshToken()) {
+                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            } else {
+                // Request authorization from the user.
+                $authUrl = $client->createAuthUrl();
+                printf("Open the following link in your browser:\n%s\n", $authUrl);
+                print 'Enter verification code: ';
+                $authCode = trim(fgets(STDIN));
+
+                // Exchange authorization code for an access token.
+                $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+                $client->setAccessToken($accessToken);
+
+                // Check to see if there was an error.
+                if (array_key_exists('error', $accessToken)) {
+                    throw new Exception(join(', ', $accessToken));
+                }
+            }
+            // Save the token to a file.
+            if (!file_exists(dirname($tokenPath))) {
+                mkdir(dirname($tokenPath), 0700, true);
+            }
+            file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+        }
+        return $client;
+}
+
 
     public function connectUsing($token)
     {
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,8 +16,17 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+         {{-- Google analytics  --}}
+         <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MH39X8L');</script>
+            <!-- End Google Tag Manager -->
 
         <!-- Scripts -->
+
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
@@ -27,5 +36,9 @@
         @env ('local')
             <script src="http://localhost:3000/browser-sync/browser-sync-client.js"></script>
         @endenv
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MH39X8L"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
     </body>
 </html>
