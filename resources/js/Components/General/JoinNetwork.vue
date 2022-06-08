205c5382eeca24c7fdb6e578496abea332c1a5ed
dr-roshyara <mathematikboy@yahoo.com>
2022-06-08 09:23:08 +0200

    header edited

--- resources/js/Components/General/JoinNetwork.vue
+++ resources/js/Components/General/JoinNetwork.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="mx-auto flex flex-col px-2 shadow-inner sm:w-full md:max-w-screen-md mb-2"
+        class="mx-auto mb-2 flex flex-col items-center justify-center px-2 shadow-inner sm:w-full md:max-w-screen-md"
     >
         <a title="NRNA Netowrk" href="/register">
             <h1
@@ -10,51 +10,66 @@
             </h1>
         </a>
 
-        <p class="mx-auto pb-2">
-            Are you interested to join the Nepalese network and interact with
-            the Nepali Diaspora? Please choose the corresponding button.
-             <div class="grid grid-cols-2 gap-x-3 py-4 mb-10 my-auto  space-y-8 justify-items-center">
-                 <!-- Registration  -->
-                 <a href="/register" class="my-auto mb-0">
-                 <jet-button>
-                     Registration
-                     </jet-button>
+        <div
+            class="mx-auto grid grid-cols-1 place-content-center justify-center pb-2"
+        >
+            <p>
+                Are you interested to join the Nepalese network and interact
+                with the Nepali Diaspora? Please choose the corresponding
+                button.
+            </p>
+            <div
+                class="my-auto mb-10 grid grid-cols-2 place-items-center gap-x-3 space-y-8 py-4"
+            >
+                <!-- Registration  -->
+                <a href="/register" class="my-auto mb-0">
+                    <jet-button> Registration </jet-button>
                 </a>
                 <!-- Direct Button  -->
-                 <a href="/login" class="my-auto mb-0 mx-auto">
-                 <jet-button>
-                     Direct Login
-                    </jet-button>
+                <a href="/login" class="my-auto mb-0">
+                    <jet-button> Direct Login </jet-button>
                 </a>
 
                 <!-- login with google  -->
-                <a href="/login/google" class="my-auto mb-0 mx-auto">
-                        <img
-                            src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png"
-                        />
+                <a href="/login/google" class="my-auto mb-0">
+                    <img
+                        src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png"
+                    />
                 </a>
-                 <!-- login in with facebook  -->
+                <!-- login in with facebook  -->
 
-                <a  href="/login/facebook" class="my-auto mb-0 mx-auto">
-                    <div class="rounded-lg  uppercase text-bold text-sm"  style="background: #4267B2; color: #ffffff; padding: 10px; width: 100%; text-align: center; display: block; border-radius:3px;">
-                        Login with Facebook
+                <a href="/login/facebook" class="my-auto mb-0 rounded">
+                    <div
+                        class="text-bold rounded-lg text-sm uppercase shadow"
+                        style="
+                            background: #4267b2;
+                            color: #ffffff;
+                            padding: 10px;
+                            width: 100%;
+                            text-align: center;
+                            display: block;
+                            border-radius: 3px;
+                        "
+                    >
+                        Facebook Login
                     </div>
                 </a>
-
             </div>
-            Inside this network, you can share, communicate, and interact with
-            the Nepalese living around the globe. Become a member to get engaged
-            with the Global Diaspora for the betterment of Nepal and Nepalese
-            communities around the world.
-        </p>
+            <p>
+                Inside this network, you can share, communicate, and interact
+                with the Nepalese living around the globe. Become a member to
+                get engaged with the Global Diaspora for the betterment of Nepal
+                and Nepalese communities around the world.
+            </p>
+        </div>
     </div>
 </template>
 <script>
 import JetButton from "@/Jetstream/Button";
 
 export default {
-   components:{
-       JetButton
-   }
+    components: {
+        JetButton,
+    },
 };
 </script>
--- resources/js/Jetstream/NrnaHeader.vue
+++ resources/js/Jetstream/NrnaHeader.vue
@@ -1,24 +1,23 @@
 <template>
     <div class="flex flex-col rounded-sm bg-indigo-800">
         <div
-            class="md:flex-warp item-center md: flex w-full flex-col justify-center rounded border border-blue-800 p-2"
+            class="flex-start flex flex-row rounded border border-blue-800 p-2"
         >
-            <div class="flex-center flex items-center justify-between">
-                <logo> </logo>
-                <div class="ml-4 w-3/4 px-2">
-                    <div
-                        class="flex flex-row items-center justify-between space-x-3 sm:mt-0"
-                    >
-                        <div class="flex flex-col">
-                            <h4
-                                class="mx-auto py-2 text-xl font-bold text-gray-50 md:text-2xl"
-                            >
-                                Non Resident Nepali Association
-                            </h4>
-                        </div>
-                        <div class="block md:hidden">
-                            <menu-icon></menu-icon>
-                        </div>
+            <logo class="flex-none"> </logo>
+
+            <div class="mx-auto px-2 text-center">
+                <div
+                    class="flex flex-row items-center justify-start space-x-3 sm:mt-0"
+                >
+                    <div class="flex flex-col">
+                        <h1
+                            class="mx-auto py-2 text-xl font-bold text-gray-50 md:text-2xl"
+                        >
+                            Non Resident Nepali Association (NRNA)
+                        </h1>
+                    </div>
+                    <div class="block md:hidden">
+                        <menu-icon></menu-icon>
                     </div>
                 </div>
             </div>
