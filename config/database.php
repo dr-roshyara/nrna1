0e9236ea18e5a7748b3fae14840afa305383b7b2
=Dr. Nab Raj Roshyara <mathematikboy@yahoo.com>
2026-04-19 01:59:08 +0200

    feat: implement Path 2 - two-database architecture (MySQL main app, PostgreSQL for Geography)

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -75,6 +75,20 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
+
+        'pgsql_geo' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_PGSQL_GEO_HOST', '127.0.0.1'),
+            'port' => env('DB_PGSQL_GEO_PORT', '5432'),
+            'database' => env('DB_PGSQL_GEO_DATABASE', 'publicdigit'),
+            'username' => env('DB_PGSQL_GEO_USERNAME', 'publicdigit_user'),
+            'password' => env('DB_PGSQL_GEO_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
